e4f834331d06fde323c9b8fd31e751390fad0313
Kateryna2882 <govoryhina82@gmail.com>
2024-07-07 14:51:53 +0300

    Add Iphone class

---
+++ src/Iphone.java
@@ -0,0 +1,19 @@
+public abstract class Iphone implements Smartphones{
+
+    @Override
+    public void call() {
+
+    }
+
+    @Override
+    public void sms() {
+
+    }
+
+    @Override
+    public void internet() {
+
+    }
+
+
+}
